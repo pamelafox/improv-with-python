14d753d8304e9fd4670a4ff586391ab10e41e8fd
Pamela Fox <pamela.fox@gmail.com>
2024-09-16 17:10:05 +0000

    More stories and images

--- sentencestory.py
+++ sentencestory.py
@@ -26,51 +26,38 @@ elif API_HOST == "github":
 console.print(f"Using {API_HOST} hosted model")
 
 
-storyspine = [
-    "Once there was… ",
-    "And every day… ",
-    "Until one day… ",
-    "And because of that… ",
-    "And because of that… ",
-    "And because of that… ",
-    "Until finally… ",
-    "And so… ",
-    "The moral of the story is: "
-]
-
 player_colors = ["green", "blue", "red", "yellow", "magenta", "cyan"]
 players = ["user"] + model_names
-#players = model_names
-random.shuffle(players)
+players = model_names
+#random.shuffle(players)
 
 current_player_ind = 0
-storyspine_index = 0
 
 
 messages = [
         {"role": "system",
         "content":
         """You are playing an Improv game where to goal is to collaboratively write a story together.
-        Only complete the sentence once you have been prompted with the start of the sentence.
+        Each round, you will come up with a new sentence to add to the story, and ONLY one sentence.
+        If it feels like the story is complete, you can end the game by typing 'The end'.
+        Most stories should only be 5-10 sentences long.
         """}
 ]
 
 while True:
-    if storyspine_index == len(storyspine):
-        print("The story is complete!")
+    if messages[-1]["content"].lower().strip(".").endswith("the end"):
         break
     
     current_player = players[current_player_ind]
     current_player_color = "black" if current_player == "user" else player_colors[current_player_ind]
     current_style = Style(color=current_player_color)
-    
     console.print(f"\n[italic]Current player: {current_player}[/italic]", style=current_style)
-    next_prompt = storyspine[storyspine_index]
+
     if current_player == "user":
-        storyline = console.input(f"\n{next_prompt}")
-        messages.append({"role": "user", "content": f"{next_prompt} {storyline}"})
+        storyline = console.input(f"\nNext sentence: ")
+        messages.append({"role": "user", "content": storyline})
     else:
-        messages.append({"role": "user", "content": f"Complete the sentence that starts with '{next_prompt}'. Include the full sentence in the response. Only respond with a single sentence."})
+        messages.append({"role": "user", "content": f"Suggest the next sentence in the story, and only one sentence. Your sentence should start with a capital letter and end with a period. Do not say anything after the period."})
         response = client.chat.completions.create(
             model=current_player,
             messages=messages,
@@ -82,5 +69,4 @@ while True:
         console.print(bot_response, style=current_style)
         
     # Switch players
-    storyspine_index += 1
     current_player_ind = (current_player_ind + 1) % len(players)
---
+++ yesandproduct.py
@@ -0,0 +1,123 @@
+import os
+import random
+import base64
+
+import openai
+from rich.console import Console
+from rich.style import Style
+
+console = Console(highlight=False)
+
+# Setup the OpenAI client to use either Ollama or GitHub
+API_HOST = "github"
+
+if API_HOST == "ollama":
+    client = openai.OpenAI(
+        base_url="http://localhost:11434/v1",
+        api_key="nokeyneeded",
+    )
+    model_names = ["llama3.1:8b", "phi3:mini"]
+elif API_HOST == "github":
+    client = openai.OpenAI(
+        base_url="https://models.inference.ai.azure.com",
+        api_key=os.getenv("GITHUB_TOKEN")
+    )
+    model_names = ["meta-llama-3-8b-instruct", "gpt-4o"]
+console.print(f"Using {API_HOST} hosted model")
+
+
+player_colors = ["green", "blue", "red", "yellow", "magenta", "cyan"]
+players = ["user"] + model_names
+# players = model_names
+# random.shuffle(players)
+
+current_player_ind = 0
+num_rounds = 5
+
+messages = [
+    {"role": "system",
+     "content":
+     """You are playing an Improv game where to goal is to collaboratively describe a new product together.
+    Each round, you will come up with a new sentence describing the product, starting with 'Yes, and...'.
+    At the end, you will come up with a great name or the product.
+    """},
+    # Few shot examples
+    {"role": "user", "content": "Introducing our brand new product, a ball that shoots laser beams!"},
+    {"role": "assistant", "content": "Yes, and it also has a built-in camera."},
+    {"role": "user", "content": "Yes, and it can fly."},
+    {"role": "assistant", "content": "Yes, and it can also be controlled with your mind."},
+]
+
+# Get the first line of the product description
+
+
+def open_image_as_base64(filename):
+    with open(filename, "rb") as image_file:
+        image_data = image_file.read()
+    image_base64 = base64.b64encode(image_data).decode("utf-8")
+    return f"data:image/png;base64,{image_base64}"
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        # Few shot examples
+        {"role": "assistant", "content": "Introducing our brand new product, a ball that shoots laser beams!"},
+        {
+        "role": "user",
+        "content": [
+            {"text": "Pretend you've NEVER EVER seen this object before, and describe it as if it is a new invention from a toy company. Use only a single sentence to describe it. Do not name it.", "type": "text"},
+            {
+                "image_url": {"url": open_image_as_base64("images/toy_soundloop.png")},
+                "type": "image_url",
+            },
+        ],
+    }],
+    temperature=0.7)
+product_description = response.choices[0].message.content
+messages.append({"role": "user", "content": product_description})
+console.print(f"What are we building? \n {product_description}")
+
+for current_round in range(num_rounds):
+
+    current_player = players[current_player_ind]
+    current_player_color = "black" if current_player == "user" else player_colors[
+        current_player_ind]
+    current_style = Style(color=current_player_color)
+    console.print(
+        f"\n[italic]Current player: {current_player}[/italic]", style=current_style)
+
+    if current_round == num_rounds - 1:
+        if current_player == "user":
+            product_name = console.input(f"\nWhat is the name of this product? Be inventive! ")
+        else:
+            messages.append({"role": "user", "content": "What is the name of this product? Be inventive! ONLY respond with the name, not the reasoning or anything else."})
+            response = client.chat.completions.create(
+                model=model_names[0],
+                messages=messages,
+                temperature=0.7,
+                max_tokens=20
+            )
+            product_name = response.choices[0].message.content
+        console.print(f"\n[bold]Product name: {product_name}[/bold]", style=current_style)
+        break
+
+    if current_player == "user":
+        storyline = console.input(f"\nYour description: ")
+        messages.append({"role": "user", "content": storyline})
+    else:
+        if current_round == num_rounds - 1:
+            messages.append({"role": "user", "content": "What is the name of this product? Be inventive!"})
+        response = client.chat.completions.create(
+            model=current_player,
+            messages=messages,
+            temperature=0.7,
+            max_tokens=100
+        )
+        bot_response = response.choices[0].message.content
+        messages.append({"role": "assistant", "content": bot_response})
+        console.print(bot_response, style=current_style)
+
+    # Switch players
+    current_player_ind = (current_player_ind + 1) % len(players)
+
+
