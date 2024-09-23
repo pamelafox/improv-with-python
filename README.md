76ab17e7a67f30baad093c939fa48051a739166d
Pamela Fox <pamela.fox@gmail.com>
2024-09-22 20:25:38 -0700

    Improve readme

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # Playing improv with Python
 
-This repository contains slides and examples from the PyBay talk "Playing improv with Python".
+This repository contains slides and games from the PyBay talk "Playing improv with Python".
+
+* [Viewing the slides](#viewing-the-slides)
+* [Running the games](#running-the-games)
+* [Available games](#available-games)
 
 ## Viewing the slides
 
-You can view the slides on GitHub pages for this repo, or by running a local Python server in the repo:
+You can view the slides [published on GitHub pages](https://pamelafox.github.io/improv-with-python/), or by running a local Python server in the repo:
 
 ```shell
 python -m http.server 8000
 ```
 
-## Running the examples
+## Running the games
 
-If you open this up in a Dev Container or GitHub Codespaces, everything will be setup for you.
+If you open this up in a VS Code Dev Container or GitHub Codespaces, everything will be setup for you.
 If not, follow these steps:
 
 1. Set up a Python virtual environment and activate it.
@@ -23,22 +27,27 @@ If not, follow these steps:
     python -m pip install -r requirements.txt
     ```
 
-3. To use GitHub models when running the examples,
+3. To use GitHub models for the games,
     you'll need a `GITHUB_TOKEN` environment variable that stores a GitHub personal access token.
     If you're running this inside a GitHub Codespace, the token will be automatically available.
     If not, generate a new [personal access token](https://github.com/settings/tokens) and run this command to set the `GITHUB_TOKEN` environment variable:
 
     ```shell
-    export GITHUB_TOKEN="<your-github-token-goes-here>"
+    export GITHUB_TOKEN="your-github-token-goes-here"
 
-4. To use Ollama for the examples, you must have Ollama installed and then pull the desired models.
+4. To use Ollama for the games, you must have Ollama installed and then pull any models required for each game.
 
     ```shell
     ollama run llama3.1:8b
     ```
 
-## Available examples
+## Available games
 
-| Example | Description | Suggested Ollama models |
-|---------|-------------|---------------------|
-| [storyspine.py](storyspine.py) | Generate a story using the Story Spine framework | `llama3.1:8b` |
+| Improv game | Python code | Notable features |
+|-------------|-------------|------------------|
+| [Story Spine](https://github.com/pamelafox/improvlists/blob/master/games/Game%3A-Group-Story-Spine.md) | [storyspine.py](storyspine.py) | Each response must start with a specific string |
+| [Sentence-at-a-time](https://github.com/pamelafox/improvlists/blob/master/games/Game%3A-Sentence-at-a-time-Story.md) | [sentencestory.py](sentencestory.py) | Specifies a phrase to end the story |
+| [Tweet generator](https://github.com/pamelafox/improvlists/blob/master/games/Game%3A-Tweet-Generator.md) | [tweetgenerator.py](tweetgenerator.py) | Uses few-shot prompting to encourage model to return single word |
+| [Yes, And Product Factory](https://github.com/pamelafox/improvlists/blob/master/games/Game%3A-Yes%2C-And...-Product-Factory.md) | [yesandproduct.py](yesandproduct.py) | Uses vision models (llava/gpt-4o) to describe prop |
+| [Three Things!](https://github.com/pamelafox/improvlists/blob/master/games/Game%3A-3-Things!.md) | [threethings_simple.py](threethings_simple.py) | Starts with a different prompt than subsequent rounds. |
+| [Three Things!](https://github.com/pamelafox/improvlists/blob/master/games/Game%3A-3-Things!.md) | [threethings_tools.py](threethings_tools.py) | Same, but uses function calling and few shots to force a list of 3 strings. |
--- tweetgenerator.py
+++ tweetgenerator.py
@@ -16,44 +16,41 @@ if API_HOST == "ollama":
     )
     model_names = ["llama3.1:8b", "phi3.5:latest"]
 elif API_HOST == "github":
-    client = openai.OpenAI(
-        base_url="https://models.inference.ai.azure.com",
-        api_key=os.getenv("GITHUB_TOKEN")
-    )
+    client = openai.OpenAI(base_url="https://models.inference.ai.azure.com", api_key=os.getenv("GITHUB_TOKEN"))
     model_names = ["meta-llama-3-8b-instruct", "gpt-4o"]
 console.print(f"Using {API_HOST} hosted model")
 
 
 player_colors = ["green", "blue", "red", "yellow", "magenta", "cyan"]
 players = ["user"] + model_names
-#players = model_names
-#random.shuffle(players)
+# players = model_names
+# random.shuffle(players)
 
 current_player_ind = 0
 
 
 messages = [
-        {"role": "system",
-        "content":
-        """You are playing an Improv game where to goal is to collaboratively write a tweet together.
+    {
+        "role": "system",
+        "content": """You are playing an Improv game where to goal is to collaboratively write a tweet together.
         Each round, you will come up with a new word to add to the tweet, and ONLY one word.
         DO NOT SUGGEST MORE THAN ONE WORD.
         If it feels like the story is tweet, you can end the tweet by suggesting a hashtag, and ONLY one hashtag.
         A tweet should be no longer than 280 characters.
         """,
-        },
-        # few shot examples
-        {"role": "user", "content": "The"},
-        {"role": "assistant", "content": "quick"},
-        {"role": "user", "content": "brown"},
-        {"role": "assistant", "content": "fox"},
-        {"role": "user", "content": "jumps"},
-        {"role": "assistant", "content": "over"},
-        {"role": "user", "content": "the"},
-        {"role": "assistant", "content": "lazy"},
-        {"role": "user", "content": "dog"},
-        {"role": "assistant", "content": "."},
-        {"role": "user", "content": "#AnimalsAreWeird"},
+    },
+    # few shot examples
+    {"role": "user", "content": "The"},
+    {"role": "assistant", "content": "quick"},
+    {"role": "user", "content": "brown"},
+    {"role": "assistant", "content": "fox"},
+    {"role": "user", "content": "jumps"},
+    {"role": "assistant", "content": "over"},
+    {"role": "user", "content": "the"},
+    {"role": "assistant", "content": "lazy"},
+    {"role": "user", "content": "dog"},
+    {"role": "assistant", "content": "."},
+    {"role": "user", "content": "#AnimalsAreWeird"},
 ]
 
 tweet_words = []
@@ -62,7 +59,7 @@ tweet_so_far = ""
 while True:
     if len(tweet_words) > 1 and tweet_words[-1]["word"].lower().strip(".").startswith("#"):
         break
-    
+
     current_player = players[current_player_ind]
     current_player_color = "black" if current_player == "user" else player_colors[current_player_ind]
     current_style = Style(color=current_player_color)
@@ -73,20 +70,28 @@ while True:
         messages.append({"role": "user", "content": word})
     else:
         if tweet_so_far == "":
-            messages.append({"role": "user", "content": "Start the tweet by suggesting the first word and ONLY one word."})
+            messages.append(
+                {"role": "user", "content": "Start the tweet by suggesting the first word and ONLY one word."}
+            )
         else:
-            messages.append({"role": "user", "content": f"Suggest the next word in the tweet, and ONLY one word. If the tweet seems over, suggest a hashtag instead. Here is the tweet so far: \"{tweet_so_far}\""})
+            messages.append(
+                {
+                    "role": "user",
+                    "content": """
+                        Suggest the next word in the tweet, and ONLY one word."
+                        If the tweet seems over, suggest a hashtag instead.
+                        Here is the tweet so far: '{tweet_so_far}'
+                        """,
+                }
+            )
         response = client.chat.completions.create(
-            model=current_player,
-            messages=messages,
-            temperature=0.3,
-            max_tokens=5
+            model=current_player, messages=messages, temperature=0.3, max_tokens=5
         )
         bot_response = response.choices[0].message.content
         messages.append({"role": "assistant", "content": bot_response})
         console.print(bot_response, style=current_style)
         word = bot_response
-    
+
     tweet_words.append({"word": word, "color": current_player_color})
     tweet_so_far += word + " "
     # Switch players
